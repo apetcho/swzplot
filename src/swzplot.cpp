ec042cd6a1010164992792678d190a6a6dbcee8d
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-25 20:04:25 +0100

    Implement CanvasBase::patch() method (3)

--- src/swzplot.cpp
+++ src/swzplot.cpp
@@ -787,6 +787,15 @@ Patch CanvasBase::patch(
     return this->gca()->add<PatchBase>()->patch(xmat, ymat, cvec);
 }
 
+// -*-
+Patch CanvasBase::patch(
+    const Matrix<double>& xmat, const Matrix<double>& ymat,
+    const Colormap& cdata
+){
+    return this->gca()->add<PatchBase>()->patch(xmat, ymat, cdata);
+}
+
+
 // -*----------------------------------------------------------------*-
 }//-*- end::namespace::swzplot                                      -*-
 // -*----------------------------------------------------------------*-
