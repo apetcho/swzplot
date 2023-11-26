0466273b093337570d2e4d6f133cfcbdc4e71462
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-26 08:13:52 +0100

    Implement swzplot::append() function (3)

--- src/swzplot.hpp
+++ src/swzplot.hpp
@@ -2110,7 +2110,11 @@ void append(Line line, std::pair<double, double> point2d){
 void append(Axes axes, std::pair<double, double> point2d){
     axes->gco<LineBase>()->vertex(point2d.first, point2d.second);
 }
-void append(Canvas canvas, std::pair<double, double> point2d);
+
+void append(Canvas canvas, std::pair<double, double> point2d){
+    canvas->gca()->gco<LineBase>()->vertex(point2d.first, point2d.second);
+}
+
 void append(Figure fig, std::pair<double, double> point2d);
 void append(Line line, double x, double y);
 void append(Axes axes, double x, double y);
