2ed97e580eccc1615c47c6285a1eff29da4afdb3
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-26 13:08:35 +0100

    Implement glut::create_window() function

--- src/swzglut.cpp
+++ src/swzglut.cpp
@@ -7,21 +7,21 @@ namespace swzplot{
 // -*-
 namespace glut{
 // -*-
-using GlutDict = std::map<int, Figure>;
+using WindowDict = std::map<int, Figure>;
 using WindowQueue = std::list<Figure>;
 
 // -*-
-void idle();
-void display();
-void no_display();
-void reshape(int width, int height);
-void mouse(int button, int state, int x, int y);
-void motion(int x, int y);
-void passivemotion(int x, int y);
-void keyboard(unsigned char key, int x, int y);
+void idlefn();
+void displayfn();
+void no_displayfn();
+void reshapefn(int width, int height);
+void mousefn(int button, int state, int x, int y);
+void motionfn(int x, int y);
+void passivemotionfn(int x, int y);
+void keyboardfn(unsigned char key, int x, int y);
 
 int toolWindowNum;
-GlutDict windowDict;        // windows
+WindowDict windowDict;      // windows
 WindowQueue windowQueue;    // 
 std::mutex wq_mutex;
 
@@ -30,6 +30,23 @@ void set_window_title(int window, std::string name){
     assert(window);
     glutSetWindow(window);
     glutSetWindowTitle(name.c_str());
+}
+
+// -*-
+void create_window(const Figure fig){
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowPosition(fig->m_windowBBox.left, fig->m_windowBBox.bottom);
+    glutInitWindowSize(fig->m_windowBBox.width, fig->m_windowBBox.height);
+    fig->m_window_num = glutCreateWindow(fig->m_window_name.c_str());
+    glutIdleFunc(idlefn);
+    glutDisplayFunc(displayfn);
+    glutReshapeFunc(reshapefn);
+    glutMotionFunc(motionfn);
+    glutMouseFunc(mousefn);
+    glutPassiveMotionFunc(passivemotionfn);
+    glutKeyboardFunc(keyboardfn);
+    glutShowWindow();
+    windowDict.insert(WindowDict::value_type(fig->m_window_num, fig));
 }
 
 // -*------------------------*-
