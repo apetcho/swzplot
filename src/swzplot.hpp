e4014ca2e3ad52fe224fd5dd711df7223dbab211
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-26 08:16:40 +0100

    Implement swzplot::append() function (7)

--- src/swzplot.hpp
+++ src/swzplot.hpp
@@ -2127,7 +2127,10 @@ void append(Axes axes, double x, double y){
     axes->gco<LineBase>()->vertex(x, y);
 }
 
-void append(Canvas canvas, double x, double y);
+void append(Canvas canvas, double x, double y){
+    canvas->gca()->gco<LineBase>()->vertex(x, y);
+}
+
 void append(Figure fig, double x, double y);
 void append(Line line, Position<double> point2d);
 void append(Axes axes, Position<double> point2d);
